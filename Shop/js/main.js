fac19a2c61de3b580e569c082518d641f84120d4
LiTeraLogic <liligimmy@mail.ru>
2020-12-25 19:56:44 +0900

    Реализовано отображение корзины при наведении и подгрузка данных для корзины с сервера

--- Shop/js/main.js
+++ Shop/js/main.js
@@ -1,19 +1,24 @@
+// 'use strict'
+
+const API = 'https://raw.githubusercontent.com/GeekBrainsTutorial/online-store-api/master/responses';
+
 class ProductsList{
     constructor(container = '.products'){
         this.container = container;
         this.goods = []; //массив товаров
-//        this.allProducts=[]; //массив объектов
-        this._fetchProducts();
+        this.allProducts=[]; //массив объектов
+        this._getProducts()
+            .then(data => { //data - объект js
+                this.goods = [...data];
+                this.render()
+            });
     }
-    _fetchProducts(){
-        this.goods = [
-            {id: 1, title: 'Notebook', price: 2000},
-            {id: 2, title: 'Mouse', price: 20},
-            {id: 3, title: 'Keyboard', price: 200},
-            {id: 4, title: 'Gamepad', price: 50},
-        ];
-
-        this.totalPrice();
+    _getProducts(){
+        return fetch(`${API}/catalogData.json`)
+            .then(result => result.json())
+            .catch(error => {
+                console.log(error);
+            })
     }
 
     // генерация товаров на страницу
@@ -39,10 +44,12 @@ class ProductsList{
 }
 
 class ProductItem{
-    constructor(product,img='https://placehold.it/200x150'){
-        this.title = product.title;
+    constructor(product, img='https://placehold.it/200x150'){
+        // this.title = product.title;
+        this.title = product.product_name;
         this.price = product.price;
-        this.id = product.id;
+        // this.id = product.id;
+        this.id = product.id_product;
         this.img = img;
     }
 
@@ -55,16 +62,6 @@ class ProductItem{
                 <button class="buy-btn">Купить</button>
             </div>`
     }
-
-    // генерация товаров на страницу
-    render(){
-        const block = document.querySelector(this.container);
-        for(let product of this.goods){
-            const productObject = new ProductItem(product);
-//            this.allProducts.push(productObject);
-            block.insertAdjacentHTML('beforeend', productObject.render());
-        }
-    }
 }
 
 /**
@@ -73,17 +70,47 @@ class ProductItem{
 class Cart{
     constructor(container = '.cart'){
         this.container = container;
-        this.goods = []; //массив товаров в корзине
-//        this.allProducts=[]; //массив объектов
+        this.catrItems = []; //массив товаров в корзине
         this.amount = 0; // сумма заказа
         this.countGoods = 0; // количество товаров в корзине
-        this._fetchProducts();
+        this._getCartItems()
+            .then(data => {
+                this.amount = data['amount'];
+                this.countGoods = data['countGoods'];
+                this.catrItems = [...data['contents']];
+            })
+            .then(() => this._render());
     }
 
-    _fetchProducts(){}
+    _getCartItems(){
+        return fetch(`${API}/getBasket.json`)
+            .then(result => result.json())
+            .catch(error => {
+                console.log(error);
+            })
+
+    }
+
 
     // генерация корзины
-    render(){}
+    _render(){
+        const block = document.querySelector(this.container);
+        for(let item of this.catrItems){
+            const productObject = new CartItem(item);
+//            this.allProducts.push(productObject);
+            block.insertAdjacentHTML('beforeend', productObject.render());
+        }
+        block.insertAdjacentHTML('beforeend', this._renderAmount());
+    }
+
+    _renderAmount(){
+        return `
+            <div class="products-price">
+                <p > Кол-во наименований: ${this.countGoods}</p>
+                 <p > Итого: ${this.amount}</p>
+            </div>
+            `;
+    }
 
     // увеличить количество товаров на 1
     addItem(){}
@@ -96,18 +123,35 @@ class Cart{
  *  Класс генерации одного элемента корзины
  */
 class CartItem{
-    constructor(product,img='https://placehold.it/200x150'){
-        this.title = product.title;
+    constructor(product, img='https://placehold.it/50x100'){
+        this.product_name = product.product_name;
         this.price = product.price;
-        this.id = product.id;
+        this.id_product = product.id_product;
         this.img = img;
-        this.count = 1;
+        this.quantity = product.quantity;
     }
 
     // генерация одного товара в корзине
-    render(){}
+    render(){
+        return `
+        <div class="cart-item">
+                <div class="left-block">
+                    <img class="cart-image" src="${this.img}" alt="image">
+                    <div class="product-list">
+                        <p class="product-name">Наименование: ${this.product_name}</p>
+                        <p class="product-price">Цена: ${this.price}</p>
+                        <p class="product-number">Количество: ${this.quantity}</p>
+                    </div>
+                </div>
+                <div class="right-block">                    
+                    <button class="delete-btn">Del</button>
+                </div>
+            </div>`;
+    }
 }
 
 let list = new ProductsList();
 list.render();
 list.totalPrice();
+
+let cart = new Cart();
